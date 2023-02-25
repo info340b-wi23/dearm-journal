61208edd03c5dd1f402931379ec145d49215e305
Daniella <daniellatsing@gmail.com>
2023-02-25 14:00:03 -0800

    installed firebase and fixed reactscript version

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App.js';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWxWuDvubVW38FuplSLu6HFTwXhJaHGiU",
+  authDomain: "dearm-journal-a829a.firebaseapp.com",
+  projectId: "dearm-journal-a829a",
+  storageBucket: "dearm-journal-a829a.appspot.com",
+  messagingSenderId: "1066196978864",
+  appId: "1:1066196978864:web:f7dfa3408067d0114dbf75"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
