bb0979879ccf2f994daa46cb0f996eb72b38a8d5
Daniella <daniellatsing@gmail.com>
2023-03-13 14:47:22 -0700

    fixed class names

--- src/components/JournalWrite.js
+++ src/components/JournalWrite.js
@@ -89,9 +89,9 @@ export function JournalWrite(props) {
                     <label className="journal-write-title" htmlFor="Title">Title:</label> 
                     <input type="text" name="title" className="title" onChange={handleTitile} value={title}/>
                     <label className="journal-write-content-header" htmlFor="Content">Content:</label> 
-                    <input type="text" name="content" className="content" onChange={handleContent} value={content}/>
+                    <input type="text" name="content" className="journal-write-content" onChange={handleContent} value={content}/>
                     <label className="journal-write-upload-label" htmlFor="Image Upload">Image Upload:</label> 
-                    <img className="upload-img" src={imagePreviewLocation} alt="dream image"/>
+                    <img className="journal-write-upload-img" src={imagePreviewLocation} alt="dream image"/>
                     {loading && <p>loading...</p>}
                     <input className="upload" type="file" name="image" id="imageUploadInput" onChange={handleImg}/>
                     <button className="journal-write-save-button" onClick={handleImageUpload}>Save</button>
